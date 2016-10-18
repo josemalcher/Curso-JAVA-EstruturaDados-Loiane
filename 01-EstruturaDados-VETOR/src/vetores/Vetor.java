49627896239a23436149b0be0d559c916e21a4a3
José Malcher Jr <malcher.malch@gmail.com>
2016-10-18 17:24:27 -0300

    Aula 07 - Est. de Dados - Vetores e Arrays - Adicionar Elementos em qualquer posição

--- 01-EstruturaDados-VETOR/src/vetores/Vetor.java
+++ 01-EstruturaDados-VETOR/src/vetores/Vetor.java
@@ -34,6 +34,23 @@ public class Vetor {
 		}
 	}
 
+	// 0 1 2 3 4 5 6 = tamanho é 5
+	// B C E F G + +
+	public boolean adiciona(int posicao, String elemento) {
+		if (!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição inválida");
+			//return false
+		}
+		
+		for(int i = this.tamanho - 1 ; i >= posicao; i--){
+			this.elementos[i + 1] = this.elementos[i];
+		}
+		this.elementos[posicao] = elemento;
+		this.tamanho++;
+		
+		return true;
+	}
+
 	public int tamanho() {
 		return this.tamanho;
 	}
@@ -72,7 +89,7 @@ public class Vetor {
 				return i;
 			}
 		}
-		return -1; //posição que não existe no vetor
+		return -1; // posição que não existe no vetor
 	}
 
 }
