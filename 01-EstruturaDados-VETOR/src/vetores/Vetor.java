e8ad8cb9b24bdbaae15fdb6342b805cccc52d88a
José Malcher Jr <malcher.malch@gmail.com>
2016-10-18 16:42:57 -0300

    Aula 04 - Est. de Dados - Vetores e Arrays - Tamanho e imprimir elementos, toString

--- 01-EstruturaDados-VETOR/src/vetores/Vetor.java
+++ 01-EstruturaDados-VETOR/src/vetores/Vetor.java
@@ -1,43 +1,60 @@
 package vetores;
 
+import java.util.Arrays;
+
 public class Vetor {
 
 	private String[] elementos;
 	private int tamanho = 0;
-	
-	public Vetor(int capacidade){
+
+	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
 	}
-	
-/*	public void adiciona(String elemento){
-		for(int i = 0 ; i < this.elementos.length ; i++){
-			if(this.elementos[i] == null){
-				this.elementos[i] = elemento;
-				break;
-			}
-		}
-	}*/
-	
-/*	public void adiciona(String elemento)throws Exception{
-		if(this.tamanho < this.elementos.length){
-			this.elementos[this.tamanho] = elemento;
-			this.tamanho++;
-		}else{
-			throw new Exception("Vetor Cheio, não é possivel adicionar mais!");
-		}
-	}
-	*/
-	
-	public boolean adiciona(String elemento){
-		if(this.tamanho < this.elementos.length){
+
+	/*
+	 * public void adiciona(String elemento){ for(int i = 0 ; i <
+	 * this.elementos.length ; i++){ if(this.elementos[i] == null){
+	 * this.elementos[i] = elemento; break; } } }
+	 */
+
+	/*
+	 * public void adiciona(String elemento)throws Exception{ if(this.tamanho <
+	 * this.elementos.length){ this.elementos[this.tamanho] = elemento;
+	 * this.tamanho++; }else{ throw new Exception(
+	 * "Vetor Cheio, não é possivel adicionar mais!"); } }
+	 */
+
+	public boolean adiciona(String elemento) {
+		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
 			this.tamanho++;
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
-	
-	
-	
+
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		
+		s.append("[");
+
+		for (int i = 0; i < this.tamanho - 1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+
+		if (this.tamanho > 0) {
+			s.append(this.elementos[this.tamanho - 1]);
+		}
+
+		s.append("]");
+
+		return s.toString();
+	}
+
 }
