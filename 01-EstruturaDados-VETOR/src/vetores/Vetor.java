adba175977a55df498f3d77571617d4e6e47bccb
José Malcher Jr <malcher.malch@gmail.com>
2016-10-18 16:27:33 -0300

    Aula 03 - Est. de Dados - Vetores e Arrays - Adicionar elementos no final do vetor - 3 maneiras

--- 01-EstruturaDados-VETOR/src/vetores/Vetor.java
+++ 01-EstruturaDados-VETOR/src/vetores/Vetor.java
@@ -3,9 +3,41 @@ package vetores;
 public class Vetor {
 
 	private String[] elementos;
+	private int tamanho = 0;
 	
 	public Vetor(int capacidade){
 		this.elementos = new String[capacidade];
 	}
+	
+/*	public void adiciona(String elemento){
+		for(int i = 0 ; i < this.elementos.length ; i++){
+			if(this.elementos[i] == null){
+				this.elementos[i] = elemento;
+				break;
+			}
+		}
+	}*/
+	
+/*	public void adiciona(String elemento)throws Exception{
+		if(this.tamanho < this.elementos.length){
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+		}else{
+			throw new Exception("Vetor Cheio, não é possivel adicionar mais!");
+		}
+	}
+	*/
+	
+	public boolean adiciona(String elemento){
+		if(this.tamanho < this.elementos.length){
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	
 	
 }
---
+++ 01-EstruturaDados-VETOR/src/vetores/teste/Aula03.java
@@ -0,0 +1,27 @@
+package vetores.teste;
+
+import vetores.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(2);
+
+/*		try {
+			vetor.adiciona("Elemento 01");
+			vetor.adiciona("Elemento 02");
+			vetor.adiciona("Elemento 03");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+
+			vetor.adiciona("Elemento 01");
+			vetor.adiciona("Elemento 02");
+			vetor.adiciona("Elemento 03");
+	
+		
+
+	}
+
+}
