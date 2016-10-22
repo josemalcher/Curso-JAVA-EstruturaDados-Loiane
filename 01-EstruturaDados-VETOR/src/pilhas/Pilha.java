f04b6d3cb972e58e861a8ef9f7d84bb94ff49bb2
José Malcher Jr <malcher.malch@gmail.com>
2016-10-22 11:52:14 -0300

    Aula 13 - Est. de Dados - PILHAS - Introdução. Primeiros ajustes e criação de uma estrutura Base

---
+++ 01-EstruturaDados-VETOR/src/pilhas/Pilha.java
@@ -0,0 +1,18 @@
+package pilhas;
+
+import base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+
+	public Pilha() {
+		super();
+	
+	}
+
+	public Pilha(int capacidade) {
+		super(capacidade);
+	}
+
+	
+	
+}
---
+++ 01-EstruturaDados-VETOR/src/pilhas/teste/Aula13.java
@@ -0,0 +1,17 @@
+package pilhas.teste;
+
+import pilhas.Pilha;
+
+public class Aula13 {
+
+	public static void main(String[] args) {
+
+		
+		Pilha<Integer> pilha = new Pilha<>(); //capacidade 10
+		
+		System.out.println(pilha);
+		System.out.println(pilha.tamanho());
+
+	}
+
+}
