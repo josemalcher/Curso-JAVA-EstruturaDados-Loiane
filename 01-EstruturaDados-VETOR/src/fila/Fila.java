a3ab8620707b963b9d44370abb163eff8b2dcecd
José Malcher Jr <malcher.malch@gmail.com>
2016-10-23 20:04:14 -0300

    Aula 19 - Est. de Dados - Fila - Introdução

---
+++ 01-EstruturaDados-VETOR/src/fila/Fila.java
@@ -0,0 +1,20 @@
+package fila;
+
+import base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T>{
+
+	public Fila() {
+		super();
+	}
+
+	public Fila(int capacidade) {
+		super(capacidade);
+	}
+	
+	
+
+	
+	
+	
+}
---
+++ 01-EstruturaDados-VETOR/src/fila/teste/Aula19.java
@@ -0,0 +1,18 @@
+package fila.teste;
+
+import fila.Fila;
+
+public class Aula19 {
+
+	public static void main(String[] args) {
+		
+		Fila<Integer> fila = new Fila<>();
+		//herança da estruturaEstatica
+		System.out.println(fila.estaVazia());
+		System.out.println(fila.tamanho());
+		
+		
+
+	}
+
+}
