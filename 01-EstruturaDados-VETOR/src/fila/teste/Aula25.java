7b1f2ce7be691b17cf0f76e85b74cf236fe56ba8
José Malcher Jr <malcher.malch@gmail.com>
2016-10-23 21:51:57 -0300

    Aula 25 - Est. de Dados - Fila - API JAVA: PriorityQueue

---
+++ 01-EstruturaDados-VETOR/src/fila/teste/Aula25.java
@@ -0,0 +1,24 @@
+package fila.teste;
+//https://www.youtube.com/watch?v=tAnXS9YyHAU
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Aula25 {
+	public static void main(String[] args) {
+
+		Queue<Paciente> filaComPrioridade = new PriorityQueue<>(new Comparator<Paciente>() {
+			//Caso não haja no método Paciente o método Comparable/compareTo
+			@Override
+			public int compare(Paciente p1, Paciente p2) {
+				return Integer.valueOf(p1.getPrioridade()).compareTo(p2.getPrioridade());
+			}
+		});
+
+		filaComPrioridade.add(new Paciente("A", 2));
+		filaComPrioridade.add(new Paciente("B", 1));
+
+		System.out.println(filaComPrioridade);
+
+	}
+}
