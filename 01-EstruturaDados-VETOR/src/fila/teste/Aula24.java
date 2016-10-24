bce5c6be1d63d1a343300a1595536c03a98a3eb2
José Malcher Jr <malcher.malch@gmail.com>
2016-10-23 21:38:25 -0300

    Aula 24 - Est. de Dados - Fila - Filas com Prioridade reteste com classe Paciente

--- 01-EstruturaDados-VETOR/src/fila/teste/Aula24.java
+++ 01-EstruturaDados-VETOR/src/fila/teste/Aula24.java
@@ -5,13 +5,17 @@ import fila.FilaComPrioridade;
 public class Aula24 {
 
 	public static void main(String[] args) {
-		
-		
-		FilaComPrioridade<Integer> fila = new FilaComPrioridade<>();
-		System.out.println(fila);
 
-		fila.enfileira(1);
-		
+		FilaComPrioridade<Paciente> fila = new FilaComPrioridade<>();
+
+		fila.enfileira(new Paciente("A", 2));
+		fila.enfileira(new Paciente("C", 1));
+		fila.enfileira(new Paciente("B", 3));
+
+		System.out.println(fila); // 1 2 3 e nao 1 3 2
+
+		System.out.println(fila.desenfileira());
+
 		System.out.println(fila);
 
 	}
